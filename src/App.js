a05f3cbb8835b2ac7c7cccb02ad27f747fe52774
F.Nik <101294373+FENiki@users.noreply.github.com>
2022-05-16 18:16:10 +0430

    oeganizing folder and function syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Expenses from "./components/Expenses";
-import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
+
+import Expenses from "./components/Expenses/Expenses";
+// import ExpenseItem from "./components/ExpenseItem";
 // import logo from "./logo.svg";
 import "./App.css";
 
-
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -20,12 +21,14 @@ function App() {
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4", 
+      id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
+
+  
 
   return (
     <div>
@@ -34,26 +37,7 @@ function App() {
           <h2 className="title">Expense Tracker</h2>
         <Expenses items = {expenses}/>
           </div>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+
       </header>
     </div>
   );
