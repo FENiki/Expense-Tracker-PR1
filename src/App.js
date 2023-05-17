670636ab85f88b5d6b8520096d43b6af2287c661
F.Nik <101294373+FENiki@users.noreply.github.com>
2023-05-18 02:01:52 +0430

    start again

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import "./App.css";
 
 const DUMMY_EXPENSES = [
   {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
   },
   {
     id: "e2",
@@ -22,11 +22,12 @@ const DUMMY_EXPENSES = [
     date: new Date(2021, 2, 12),
   },
   {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
   },
+  
   {
     id: "e4",
     title: "New Desk (Wooden)",
